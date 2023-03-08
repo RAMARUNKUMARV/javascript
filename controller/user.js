361210b7609edb4b678e45fbec66554e96e3edb3
Daniel Appasamy F <danielappasamy@datayaan.com>
2023-03-08 22:07:26 +0530

    [ADD]implemented hash password in create user

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,9 @@
  
 const userSchema = require('../models/user')
-const JWT = require('../MiddleWare/authentication')
+const JWT = require('../MiddleWare/authentication');
+const { text } = require('body-parser');
+const bcrypt = require("bcrypt")
+
 
 //create User
 async function createUser (req, res) {
@@ -9,10 +12,13 @@ try {
     if(user){
     return res.status(500).json({ error: "email already exits" });
     }else{
-      const blog = await userSchema.create(req.body);
-      return res.json({ data: blog, status: "success" });
+     let password = await bcrypt.hash(req.body.password, 10)
+      req.body.password=password
+      const user = await userSchema.create(req.body);
+      return res.json({ data: user, status: "success" });
     } 
  }
+
  catch (err) {
       res.status(500).json({ error: err.message });
     }
@@ -23,9 +29,10 @@ try {
 async function getAllUser (req, res) {
     //try will execute successful
   try {
-    const blogs = await userSchema.find();
-    res.json({ data: blogs, status: "success" });
+    const users = await userSchema.find();
+    res.json({ data: users, status: "success" });
   } 
+
   
   // catch will return error
   catch (err) {
@@ -38,8 +45,8 @@ async function getAllUser (req, res) {
 //get user by id
 async function getUserById (req, res) {
   try {
-    const blog = await userSchema.findById(req.params.id);
-    res.json({ data: blog, status: "success" });
+    const user = await userSchema.findById(req.params.id);
+    res.json({ data: user, status: "success" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -49,8 +56,8 @@ async function getUserById (req, res) {
 //update user
 async function  updateUser  (req, res)  {
   try {
-    const blog = await userSchema.updateOne(req.params.id, req.body);
-    res.json({ data: blog, status: "success" });
+    const user = await userSchema.updateOne(req.params.id, req.body);
+    res.json({ data: user, status: "success" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -60,18 +67,19 @@ async function  updateUser  (req, res)  {
 //delete user
 async function deleteUser (req, res) {
   try {
-    const blog = await userSchema.deleteOne(req.params.id);
-    res.json({ data: blog, status: "success" });
+    const user = await userSchema.deleteOne(req.params.id);
+    res.json({ data: user, status: "success" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
+//CurrentUser
 async function CurrentUser(req,res){
 
     let userID = await JWT.verifyToken(req,res)
    return res.status(200).json({ message: userID });
-   
+
 }
 
 module.exports={createUser,updateUser,getAllUser,getUserById,deleteUser,CurrentUser}//export api's
