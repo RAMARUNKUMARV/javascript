951df7fd92ce7cfb6218edbf77406d10986eeda0
Daniel Appasamy F <danielappasamy@datayaan.com>
2023-03-09 10:40:08 +0530

    [change]changes in main file

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 require('./mongoose/mongoose')//database connecting
 const morgan=require('morgan')//api hits time
 const express=require('express')//frame work
-const dotenv=require('dotenv')
+const dotenv=require('dotenv')//config env file
 var app=express()
 
 //routes
-const blog = require('./routes/user')
+const user = require('./routes/user')
 dotenv.config()
 //middleware
 app.use(express.json())
@@ -14,6 +14,6 @@ app.use(morgan("common"))
 app.use(express.urlencoded({extended:true}))//en coded body
 const PORT=8000
 
-app.use('/v1/',blog)//groups
+app.use('/v1/',user)//groups
 
 app.listen(PORT,function(){console.log( PORT,'service Started ')})//server port running
